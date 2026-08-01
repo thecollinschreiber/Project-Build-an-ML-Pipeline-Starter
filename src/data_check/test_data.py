a17982d82a8b646b85bac908cd1a29f44e700643
thecollinschreiber <thecollinschreiber@gmail.com>
2026-08-01 22:57:34 +0000

    Linked data_check to pipeline in main and implemented data validation in test_data.py

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -84,6 +84,18 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert np.isfinite(kl_div) and kl_div < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+# Implementing test_row_count
+def test_row_count(data: pd.DataFrame) -> None:
+    """
+    Test to ensure dataset has between 15,000-1,000,000 rows
+    """
+    assert 15000 <= data.shape[0] <= 1000000
+
+# Implementing test_price_range
+def test_price_range(
+    data: pd.DataFrame, 
+    min_price: float, 
+    max_price: float, 
+) -> None:
+    """Test that prices fall into configured price range"""
+    assert data["price"].between(min_price,max_price).all()
